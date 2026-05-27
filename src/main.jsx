0df29e662aea91501c6fde5ca61bf11350abe301
Alfreeeed <luis.barradas.513@gmail.com>
2026-05-27 10:24:43 -0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-const root = createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(App)
+  )
 );
